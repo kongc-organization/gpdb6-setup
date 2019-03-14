467a5b0238249c38ab2f0e01542a06e57cd0da74
kongyew <kongyew@gmail.com>
2019-03-14 13:40:13 -0700

    updated

--- scripts/opt-in/java.sh
+++ scripts/opt-in/java.sh
@@ -1,10 +1,10 @@
-echo
-echo "Installing Java JDK 8"
+log
+log "Installing Java JDK 8"
 sudo yum -y install java-1.8.0-openjdk-1.8.0*
 echo "localhost" > seghostfile
 gpssh -e -v -f seghostfile sudo yum -y install java-1.8.0-openjdk-1.8.0*
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:jre/bin/java::")
-echo "JAVA HOME: $JAVA_HOME"
+log "JAVA HOME: $JAVA_HOME"
 
-echo "export JAVA_HOME=$JAVA_HOME" >> /home/gpadmin/.bashrc
+log "export JAVA_HOME=$JAVA_HOME" >> /home/gpadmin/.bashrc
 gpssh -e -v -f seghostfile "export JAVA_HOME=$JAVA_HOME >> /home/gpadmin/.bashrc"
